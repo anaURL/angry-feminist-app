f94423d3f71e996085d436b4337534e73b53dbd9
Ana URL <ana.urlic@gmail.com>
2022-05-06 19:25:04 +0200

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,87 @@
+const Data = [
+    {
+            quote:"What will liberate women, therefore, from male control is the total elimination of the money-work system, not the attainment of economic equality with men within it.",
+            author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"The male is a biological accident: the Y (male) gene is an incomplete X (female) gene, that is, it has an incomplete set of chromosomes. In other words, the male is an incomplete female, a walking abortion, aborted at the gene stage. To be male is to be deficient, emotionally limited; maleness is a deficiency disease and males are emotional cripples.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"A woman not only takes her identity and individuality for granted, but knows instinctively that the only wrong is to hurt others, and that the meaning of life is love.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"The male claim that females find fulfillment through motherhood and sexuality reflects what males think they'd find fulfilling if they were female.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"Life in this society being, at best, an utter bore and no aspect of society being at all relevant to women, there remains to civic-minded, responsible, thrill-seeking females only to overthrow the government, eliminate the money system, institute complete automation and destroy the male sex.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"In actual fact, the female function is to explore, discover, invent, solve problems crack jokes,make music -- all with love. In other words, create a magic world.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"There's no reason why a society consisting of rational beings capable of empathizing with each other, complete and having no natural reason to compete, should have a government, laws, or leaders.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+    {
+           quote:"A true community consists of individuals -- not mere species members, not couples -- respecting each others individuality and privacy, at the same time interacting with each other mentally and emotionally -- free spirits in free relation to each other -- and co-operating with each other to achieve common ends.",
+           author:"Valerie Solanas, SCUM Manifesto"},
+
+    {
+            quote:"Masculine and feminine roles are not biologically fixed but socially constructed.",
+            author:"Judith Butler"},
+   {
+            quote:"When I dare to be powerful—to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.",
+            author:"Audre Lorde"},
+    {
+            quote:"I have come to believe over and over again that what is most important to me must be spoken, made verbal and shared, even at the risk of having it bruised or misunderstood..",
+            author:"Audre Lorde"},
+     {
+             quote:"There is no such thing as a single-issue struggle because we do not live single-issue lives.",
+             author:"Audre Lorde"},
+
+      {
+              quote:"You have to act as if it were possible to radically transform the world. And you have to do it all the time.",
+              author:"Angela Davis"},
+
+       {      quote:"I believe in making contradictions productive, not in having to choose one side or the other side. As opposed to choosing either or, choosing both.",
+              author:"Angela Davis"},
+
+       {      quote:"...we have to learn how to think and act and struggle against that which is ideologically constituted as 'normal.",
+              author:"Angela Davis"},
+
+       {      quote:"Feminism involves so much more than gender equality. And it involves so much more than gender.",
+              author:"Angela Davis"},
+
+       {      quote:"Black feminism emerged as a theoretical and practical effort demonstrating that race, gender, and class are inseparable in the social worlds we inhabit.",
+              author:"Angela Davis"},
+
+       {      quote:"Progressive art can assist people to learn not only about the objective forces at work in the society in which they live, but also about the intensely social character of their interior lives.",
+              author:"Angela Davis"},
+
+       {      quote:"True resistance begins with people confronting pain...and wanting to do something to change it.",
+              author:"bell hooks"},
+
+       {      quote:"One of the most vital ways we sustain ourselves is by building communities of resistance, places where we know we are not alone.",
+              author:"bell hooks"},
+
+              
+       {      quote:"If we want a beloved community, we must stand for justice.",
+              author:"bell hooks"},
+
+       {      quote:"I will not have my life narrowed down. I will not bow down to somebody else's whim or to someone else's ignorance.",
+              author:"bell hooks"},
+
+       {      quote:"When we choose to love, we choose to move against fear, against alienation and separation. The choice to love is a choice to connect, to find ourselves in the other",
+              author:"bell hooks"},
+
+       {      quote:"I don’t say women’s rights—I say the constitutional principle of the equal citizenship stature of men and women.",
+              author:"Ruth Bader Ginsburg"},
+
+       {      quote:"The decision whether or not to bear a child is central to a woman’s life, to her wellbeing and dignity. It’s a decision she must make for herself",
+              author:"Ruth Bader Ginsburg"},
+
+        {      quote:"The decision whether or not to bear a child is central to a woman’s life, to her wellbeing and dignity. It’s a decision she must make for herself",
+              author:"Ruth Bader Ginsburg"},
+
+   
+]
+
+export default Data;
---
+++ src/List.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Data from "./Data"
+import "./index.css"
+
+const List = () => {
+  const [state, setstate] = useState(0);
+  const { quote, author } = Data[state];
+  const [hex, setHex] = useState("black");
+
+  const NextQuote = () => {
+    setstate(Math.floor(Math.random() * Data.length));
+    // setHex("#" + Math.floor(Math.random() * 16777215).toString(16));
+  };
+
+  return (
+    <>
+      <div className="container" >
+        <div className="row" id="random">
+
+          <h1 className= "h1 col-12">
+            <span> angry </span> feminist wisdom generator
+          </h1>
+
+          {/* <div className="col-md-3 col-sm-2"></div>  API*/} 
+         
+             <div className="col-md-6 col-sm-8" id="quote">
+                <div className="quote" >
+                 {quote}
+              </div>
+            
+            <div className="author text-end py-3">
+                    <div className="author"> </div>
+              --- {author}
+            </div>
+
+             <div className="text-center my-2">
+                             <button id="btn" className="py-2 px-3" onClick={NextQuote}>
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </>
+  );
+};
+
+export default List;
